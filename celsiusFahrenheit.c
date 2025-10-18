5ea2feaf8db1e2c57e72ab15166bd969b6b326bb
ShadowDrake21 <dimka670020040@gmail.com>
2025-10-18 10:40:10 +0200

    1.2 Variables and arithmetic expressions

---
+++ celsiusFahrenheit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+main()
+{
+  float celsius, fahr;
+  float lower, upper, step;
+
+  lower = 0;
+  upper = 100;
+  step = 10;
+
+  celsius = lower;
+  printf("Celsius to Fahrenheit conversion table:\n");
+  while (celsius <= upper)
+  {
+    fahr = (9.0 / 5.0) * celsius + 32.0;
+    printf("%3.0f %6.1f\n", celsius, fahr);
+    celsius = celsius + step;
+  }
+}
